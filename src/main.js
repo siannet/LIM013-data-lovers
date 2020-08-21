e05be5118de5559dd70c2b3a7115e77f1fd159b5
emae1712 <melissa.aechegaray@gmail.com>
2020-08-21 14:34:07 -0500

    funcionalidades de tipo búsqueda y PC, HTML y CSS edicion de la clase windown por window

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import { pokedex, tipo } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(data, tipo);
+/*console.log(data, tipo);*/
 
-document.getElementById("root").innerHTML= data.pokemon.map(pokedex).join(" ");
-
+const root = document.getElementById("root");
+let search= document.querySelector(".search");
+const type=document.querySelector(".type");
+const pc=document.querySelector(".pc");
 const window= document.getElementById("window");
 const btnClick= document.getElementById("root");
 const btnClose= document.getElementById("close");
+
 
 btnClick.addEventListener("click",function() {
     // console.log("funciono");
@@ -20,5 +23,41 @@ btnClose.addEventListener("click",function() {
     window.style.display="none";
 })
 
+root.innerHTML=  data.pokemon.map(pokedex).join(" ");
+/*----------------Realizar la búsqueda de pokemon----------------------*/
 
-// const listPokemon= document.getElementById("root").innerHTML=  data.pokemon.map(pokedex).reverse().join(" ");
+search.addEventListener('keyup',()=>{  //mejorar la búsqueda por nro de pokedex y autocompletado
+    const inputSearch= data.pokemon.filter((character)=>{
+        return character.name.startsWith(search.value.toLowerCase())||
+        character.num.startsWith(search.value);
+    });
+
+    root.innerHTML="";
+    root.innerHTML=inputSearch.map(pokedex).join(" ");
+});
+
+/*-------------------Seleccionar por tipo------------------------------*/
+
+type.addEventListener('change',()=>{ //change averiguar, mejorar los filtros en todos los tipos
+    const inputType= data.pokemon.filter((character)=>{
+        return character.type.includes(type.value);
+    });
+    
+    root.innerHTML=inputType.map(pokedex).join(" ");
+});
+
+
+/*---------------------Seleccionar por PC--------------------------------*/
+
+pc.addEventListener('change',()=>{ 
+    const PC= data.pokemon.sort((a,b)=>{
+        
+        if(pc.value==="HIGHER"){
+            return Object.values(b.stats)[3]-Object.values(a.stats)[3];
+        }else{
+            return Object.values(a.stats)[3]-Object.values(b.stats)[3];
+        }
+       
+   });
+    root.innerHTML=PC.map(pokedex).join(" ");
+});
