1242d0d3cbd7025cc5743bafe3de7ac5c833f966
emae1712 <melissa.aechegaray@gmail.com>
2020-09-01 12:58:37 -0500

    Class name cambiados y purgando un poco  la info

--- src/data.js
+++ src/data.js
@@ -1,24 +1,6 @@
-/*--------------Llamar datos de la Pokedex---------------------*/
-export const pokedex = (datos) => {
-  let imgTipo= "";
-  datos.type.forEach((tipo)=>{
-    imgTipo += `<img class="imgType" src="./img/${tipo}.png"/>`;
-  });
 
-    return `
-    <div class="pokedex" id= ${datos.num} >
-      <p>N° ${parseInt(datos.num)}</p>
-      <p><strong>${datos.name.toUpperCase()}</strong></p> 
-      <img src="${datos.img}">
-      <p>Tipo: ${imgTipo}</p>
-      <p> PC: ${Object.values(datos.stats)[3]}</p>
-    </div>
-    `
-}
 
 /*------------------Agrupar por tipo--------------------------*/
-export const typeOption =(input) => datos=> datos.type.includes(input);
-
 export const filterTypeOption = (data, nameType) => {
   return data.filter((objPokemon) => {
     return objPokemon.type.includes(nameType)
@@ -27,14 +9,6 @@ export const filterTypeOption = (data, nameType) => {
  //input es el parametro q será reemplazado por type.value
 
 /*------------------Agrupar por PC-----------------------------*/
-export const pcOption =(input) => (dato1,dato2)=>{ 
-  if(input==="HIGHER"){
-      return Object.values(dato2.stats)[3]-Object.values(dato1.stats)[3];
-  }else if(input==="LESS"){
-      return Object.values(dato1.stats)[3]-Object.values(dato2.stats)[3];
-  }
-}
-
 export const sortPcOption = (data, namePC)=>{
   return data.sort((objPokemon1,objPokemon2 )=>{
     if(namePC==="HIGHER"){
@@ -46,13 +20,6 @@ export const sortPcOption = (data, namePC)=>{
 }
 
 /*-----------------Ingreso de búsqueda------------------------*/
-export const inputSearch= (input) => (datos)=>{
-  let pokedexNumber =input.padStart(3,"000"); 
-  return datos.name.startsWith(input.toLowerCase())||
-        datos.num.startsWith(pokedexNumber)||
-        datos.num.startsWith(input);
-};
-
 export const filterInputSearch = (data, input) =>{
   return data.filter((objPokemon)=>{
     let pokedexNumber = input.padStart(3,"000"); 
--- src/main.js
+++ src/main.js
@@ -1,23 +1,52 @@
-import { pokedex, typeOption, pcOption, inputSearch, filterTypeOption, sortPcOption, filterInputSearch } from './data.js';
+import { filterTypeOption, sortPcOption, filterInputSearch } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-/*console.log(data, tipo);*/
-
 let root = document.getElementById("root");
-let search= document.querySelector(".search");
-const type=document.querySelector(".type");
-const pc=document.querySelector(".pc");
-const moreInformation= document.getElementById("moreInformation");
-const btnClose= document.getElementById("close");
+let search = document.querySelector(".search");
+const type = document.querySelector(".type");
+const pc = document.querySelector(".pc");
+const moreInformation = document.getElementById("moreInformation");
+const btnClose = document.getElementById("close");
 
-const characteristics = (dataIndex) => {
-    let imgTipo= "";
-    dataIndex.type.forEach((tipo)=>{
+/*--------------Mostrar datos de la Pokedex---------------------*/
+const pokedex = (datos) => {
+  let imgTipo = "";
+  datos.type.forEach((tipo) => {
     imgTipo += `<img class="imgType" src="./img/${tipo}.png"/>`;
   });
-    return `
+
+  return `
+      <div class="boxPokedex">
+        <div class="cardPokedex">
+          <div class="card-front">
+            <p>N° ${parseInt(datos.num)}</p>
+            <p><strong>${datos.name.toUpperCase()}</strong></p> 
+            <img src="${datos.img}">
+            <p>Tipo: ${imgTipo}</p>
+            <p> PC: ${Object.values(datos.stats)[3]}</p>
+          </div>
+          <div class="card-back">
+            <p>N° ${parseInt(datos.num)}</p>
+            <p><strong>${datos.name.toUpperCase()}</strong></p> 
+            <img src="${datos.img}">
+            <p>Tipo: ${imgTipo}</p>
+            <p> PC: ${Object.values(datos.stats)[3]}</p>
+            <button onclick="document.getElementById('demo').play()" id= "${datos.num}" type="button">+</button>
+          </div>
+        </div>
+      </div>
+      `
+}
+
+/*--------------Mostrar características de la Pokedex---------------------*/
+const characteristics = (dataIndex) => {
+  let imgTipo = "";
+  dataIndex.type.forEach((tipo) => {
+    imgTipo += `<img class="imgType" src="./img/${tipo}.png"/>`;
+  });
+  return `
     <div class="characteristics">
       <img src="${dataIndex.img}">
       <p>${dataIndex.num} <strong>${dataIndex.name.toUpperCase()}</strong></p>
@@ -30,100 +59,86 @@ const characteristics = (dataIndex) => {
 }
 
 /*----------------Presentación de la Pokedex----------------------*/
-root.innerHTML=  data.pokemon.map(pokedex).join(" ");
+root.innerHTML = data.pokemon.map(pokedex).join(" ");
 
 /*-----------------Ventana modal---------------------------------*/
-root.addEventListener('click',(e)=>{
-    
-    for(let j = 0; j < data.pokemon.length; j++){
-        if(data.pokemon[j].num==e.target.id){
-        modalWindow.style.display="block";
-        moreInformation.innerHTML=characteristics(data.pokemon[j]);
-        }
+root.addEventListener('click', (e) => {
+  for (let j = 0; j < data.pokemon.length; j++) {
+    if (data.pokemon[j].num == e.target.id) {
+      modalWindow.style.display = "block";
+      moreInformation.innerHTML = characteristics(data.pokemon[j]);
     }
- });
+  }
+});
 
- btnClose.addEventListener("click",function() {
-    modalWindow.style.display="none";
+btnClose.addEventListener("click", function () {
+  modalWindow.style.display = "none";
 })
 
 /*---------------------Filtrar por Tipo--------------------------------*/
-let changeTypeEvent=()=>{ 
-    const dataFiltradaPorTipo = filterTypeOption(data.pokemon, type.value);
-    const tipoOrdenadoPorPC = sortPcOption(dataFiltradaPorTipo, pc.value); //lee el filtro PC cuando se active
-    const dataOrdenadaPorPC = sortPcOption(data.pokemon, pc.value);
-
-    //const typeFiltered= dataFiltradaPorTipo.sort(pcOption(pc.value)); 
-    
-    //Muestra la cantidad por tipo seleccionado + los Pokemon
-    root.innerHTML=`
+let changeTypeEvent = () => {
+  const dataFiltradaPorTipo = filterTypeOption(data.pokemon, type.value);
+  const tipoOrdenadoPorPC = sortPcOption(dataFiltradaPorTipo, pc.value); //lee el filtro PC cuando se active
+  const dataOrdenadaPorPC = sortPcOption(data.pokemon, pc.value);
+  
+  //Muestra la cantidad por tipo seleccionado + los Pokemon
+  root.innerHTML = `
     <div class="typeQuantity">
       <p>En el tipo <strong>${type.value.toUpperCase()}</strong> hay ${Object.entries(dataFiltradaPorTipo).length} Pokemon</p>
     </div>
     ` + dataFiltradaPorTipo.map(pokedex).join(" ");
 
-    //Muestra todos los tipos, es decir la Pokedex inicial u ordena la Pokedex sin ningun tipo seleccionado
-    if(type.value=="all-types" || type.value==""){
-        root.innerHTML=dataOrdenadaPorPC.map(pokedex).join(" "); //se puso sort para que cambiara por PC
-    }
+  //Muestra todos los tipos, es decir la Pokedex inicial u ordena la Pokedex sin ningun tipo seleccionado
+  if (type.value == "all-types" || type.value == "") {
+    root.innerHTML = dataOrdenadaPorPC.map(pokedex).join(" "); //se puso sort para que cambiara por PC
+  }
 }
-type.addEventListener('change',changeTypeEvent);
+type.addEventListener('change', changeTypeEvent);
 
 /*---------------------Ordenar por PC--------------------------------*/
-let changePcEvent=()=>{
-    
-    changeTypeEvent(); // permite que trabajen en conjunto
-
-    /*
-    const dataOrdenadaPorPC = sortPcOption(data.pokemon, pc.value);
-    //Muestra MaxCP de forma independiente, cuando la pestaña Tipo este desactivada
-    if(type.value==""){
-    root.innerHTML=dataOrdenadaPorPC.map(pokedex).join(" ");
-    }*/
-}
-pc.addEventListener('change', changePcEvent);
+pc.addEventListener('change', () => {
+  changeTypeEvent(); // permite que trabajen en conjunto
+});
 
 /*----------------Realizar la búsqueda de pokemon----------------------*/
-search.addEventListener('keyup',()=>{  //mejorar la búsqueda por nro de pokedex y autocompletado
-    //const searchFiltered= data.pokemon.filter(inputSearch(search.value));
-    //const allFilters= searchFiltered.filter(typeOption(type.value)).sort(pcOption(pc.value)); //lee los filtros activos
-    const searchFiltered = filterInputSearch(data.pokemon,search.value);
-    const dataFiltradaPorTipo = filterTypeOption(searchFiltered, type.value);
-    //const tipoOrdenadoPorPC = sortPcOption(dataFiltradaPorTipo, pc.value);
-    
-    root.innerHTML="";
-    console.log(searchFiltered);
-    if(Object.entries(searchFiltered).length === 0){         //Si el usuario ingresa mal el nombre del Pokemon
-        root.innerHTML=`
+search.addEventListener('keyup', () => {  //mejorar la búsqueda por nro de pokedex y autocompletado
+  const searchFiltered = filterInputSearch(data.pokemon, search.value);
+  const dataFiltradaPorTipo = filterTypeOption(searchFiltered, type.value);
+
+  root.innerHTML = "";
+  console.log(searchFiltered);
+  if (Object.entries(searchFiltered).length === 0) {         //Si el usuario ingresa mal el nombre del Pokemon
+    root.innerHTML = `
         <div class="pikachu-search">
           <p>Pokemon no encontrado, por favor verifique</p>
           <img src="./img/Buscador.gif">
         </div>
         `
 
-    }else if(Object.entries(searchFiltered).length === 251){ //cuando se borra la busqueda
-        root.innerHTML=  dataFiltradaPorTipo.map(pokedex).join(" ")|| //búsqueda con filtros activos
-                         searchFiltered.map(pokedex).join(" ");//búsqueda independiente
-    
-    }else{
-        root.innerHTML=searchFiltered.map(pokedex).join(" ");   //búsqueda inicial
-    }   
-    //console.log(searchFiltered)
+  } else if (Object.entries(searchFiltered).length === 251) { //cuando se borra la busqueda
+    root.innerHTML = dataFiltradaPorTipo.map(pokedex).join(" ") || //búsqueda con filtros activos
+      searchFiltered.map(pokedex).join(" ");//búsqueda independiente
+
+  } else {
+    root.innerHTML = searchFiltered.map(pokedex).join(" ");   //búsqueda inicial
+  }
+  //console.log(searchFiltered)
 });
 
 
 
 
-        
+
 
 /*-----------------Ventana modal---------------------------------*/
 /*let pokemon = root.querySelectorAll(".pokedex");
 console.log(pokemon);
 for(let i = 0; i < pokemon.length; i++){
-   
+
     pokemon[i].addEventListener('click',function (){
         modalWindow.style.display="block";
         moreInformation.innerHTML=characteristics(data.pokemon[i]);
     });
 }
+
 */
